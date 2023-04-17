168287bdd33004e3c37116857471c312d5c9b341
anhle1408 <125849741+anhle1408@users.noreply.github.com>
2023-04-17 18:30:39 +0700

    anh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/home/Homepage";
-import Blogs from "./pages/blogs/BlogsPage";
-import Contact from "./pages/contact/ContactPage";
+import Personnel from "./pages/personnel/personnelPage";
 import NoPage from "./pages/not-found/not-FoundPage";
 import Login from "./pages/login/LoginPage";
+import './index.css';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         < Route path="login" element={<Login />} />
-        < Route path="contact" element={<Contact />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} /> 
+          <Route path="personnel" element={<Personnel />} />
         </Route>
         <Route path="*" element={<NoPage />} />
       </Routes>
--- frontend/src/pages/home/Homepage.js
+++ frontend/src/pages/home/Homepage.js
@@ -1,6 +1,30 @@
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
-  
+  return (
+    <div>
+     <div class="container">
+        <div class="row" >
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card" id="tpc">
+              <h4 class="card-titel">Lịch làm việc</h4>
+              <img src="https://vapa.vn/wp-content/uploads/2022/12/anh-3d-thien-nhien.jpeg" alt="" class="card Img-top" id="cd-contens" />
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card" id="tpc">
+              <h4 class="card-titel">Tuyển Dụng</h4>
+              <img src="/Img/anh-tuyen-dung.jpg" alt="" class="card Img-top" id="cd-contens" />
+            </div>
+          </div>
+          <div class="col-md-4 py-3 py-md-0">
+            <div class="card" id="tpc">
+              <h4 class="card-titel">Chấm Công</h4>
+              <img src="/Img/may-cham-cong-3.jpg" alt="" class="card Img-top" id="cd-contens" />
+            </div>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
+}
+
+export default Home;
