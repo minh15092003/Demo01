b7cccc0d77361be303b92c09b376f72a17fc92e4
= <=>
2023-04-19 11:33:09 +0700

    minhcommit

--- frontend/src/layouts/Layout.js
+++ frontend/src/layouts/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from "react-router-dom";
-import ChangePassWordModal from "./components/ChangePassWordModal"
+import ChangePassWordModal from "../components/ChangePassWordModal"
 
 const Layout = () => {
 
--- frontend/src/pages/home/Homepage.js
+++ frontend/src/pages/home/Homepage.js
@@ -1,24 +1,6 @@
 const Home = () => {
   return (
     <div>
-      {/* <div class="slideshow">
-        <div class="slider">
-          <div class="item">
-            <img className="itemim" src="https://images.unsplash.com/photo-1445768593937-05a3f7832b68?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=700&h=500&q=80" alt="" />
-          </div>
-          <div class="item">
-            <img className="itemim" src="https://images.unsplash.com/photo-1443363742879-63e3d75de2f8?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=700&h=500&q=80" alt="" />
-          </div>
-          <div class="item">
-            <img className="itemim" src="https://images.unsplash.com/photo-1445964047600-cdbdb873673d?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=700&h=500&q=80" alt="" />
-          </div>
-          <div class="item">
-            <img className="itemim" src="https://images.unsplash.com/photo-1446329813274-7c9036bd9a1f?dpr=1&auto=compress,format&crop=entropy&fit=crop&w=700&h=500&q=80" alt="" />
-          </div>
-        </div>
-      </div> */}
-
-
       <div class="main-content">
 
         <img src="https://htmediagroup.vn/wp-content/uploads/2021/06/Anh-profile-122.jpg" alt="" class="card-baner" />
